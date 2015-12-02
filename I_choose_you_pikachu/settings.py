f3595261181c66701410ff174420ff02d0fca9b5
mingkim <ming3772@gmail.com>
2015-12-02 22:49:59 +0900

    ADD - index page view function

--- I_choose_you_pikachu/settings.py
+++ I_choose_you_pikachu/settings.py
@@ -121,5 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'I_choose_you_pikachu', 'static'),
+]
 
 AUTH_USER_MODEL = 'main.User'
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    context = {
+
+    }
+    return render(request, 'main/pages/index.html', context)
