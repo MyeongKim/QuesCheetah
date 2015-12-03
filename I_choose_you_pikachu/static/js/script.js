6dfb3bdec4a12e3b5afb0607fd462f905e14dfe3
mingkim <ming3772@gmail.com>
2015-12-03 11:16:48 +0900

    ADD - index page navbar , jumbotron

---
+++ I_choose_you_pikachu/static/js/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by nuko on 2015. 12. 3..
+ */
--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from main.models import User
 
+# TODO signup 비밀번호 체크
+# TODO signup 에러 메시지 발생
+
 
 class UserForm(forms.ModelForm):
     class Meta:
