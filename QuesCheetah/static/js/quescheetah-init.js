9562819eed2a17bc0e18e97e3dc7227cf4323396
mingkim <ming3772@gmail.com>
2016-03-10 17:56:59 +0900

    UPDATE - replace locahost

--- QuesCheetah/static/js/quescheetah-init.js
+++ QuesCheetah/static/js/quescheetah-init.js
@@ -2,7 +2,7 @@ import "./quescheetah-sdk-0.1.0.js";
 
 var config = {
     'apiKey': '4ae1772ba6c8439a8249901cdeffa7fe10900595',
-    'callBackUrl': 'http://localhost:8000',
+    'callBackUrl': 'http://www.quescheetah.com',
     'receiveRealtimeResponse': false
 };
 
