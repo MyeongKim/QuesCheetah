92cff81fb1413da928b5b68ecbe3cb65e7ee1015
mingkim <ming3772@gmail.com>
2016-03-11 23:27:38 +0900

    ADD - add domain table in admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from main.models import User, ApiKey
+from main.models import User, ApiKey, Domain
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(ApiKey)
+admin.site.register(Domain)
