c08b59c5b2408a9326b6492b3e0f13d4d638bb03
mingkim <ming3772@gmail.com>
2016-03-12 13:45:29 +0900

    ADD - add models to admin

--- vote/admin.py
+++ vote/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from vote.models import Question, Answer
+from vote.models import Question, Answer, MultiQuestion, UserAnswer
 
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(MultiQuestion)
+admin.site.register(UserAnswer)
